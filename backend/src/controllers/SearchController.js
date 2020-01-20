99098613dab2f880a70b796564607e622f595432
Fabricio Braga <fabriciobrg@gmail.com>
2020-01-20 12:02:41 -0300

    Code Refactoring creating parsing function and removing it form DevController

--- backend/src/controllers/SearchController.js
+++ backend/src/controllers/SearchController.js
@@ -1,10 +1,29 @@
 const Dev = require('../models/Dev');
+const parseStringAsArray = require('../utils/parseStringAsArray');
 
 module.exports = {
     async index(request, response) {
-        console.log(request.query);
+        const { latitude, longitude, techs } = request.query;
+
+        const techsArray = parseStringAsArray(techs);
+
+        const devs = await Dev.find( {
+            techs: {
+                $in: techsArray, //"$in" is an logical operator of MongoDB
+            },
+            location: {
+                $near: {
+                    $geometry: {
+                        type: 'Point',
+                        coordinates: [longitude, latitude],
+                    },
+                    $maxDistance: 10000, //Distance in meters
+                },
+            },
+        });
+        console.log(techsArray);
         //Buscar todos devs num raio 10km
         //Filtrar por tecnologias
-        return response.json({ devs: [] });
+        return response.json({ devs });
     }
 }
